76233324595d8c557a141fb16487272f470c672a
ABDUL WAHAB SHAHID <ab.1303@gmail.com>
2022-02-08 14:32:55 +1100

    feat(restaurant-edit)!: some styling improvements

--- src/pages/settings/restaurants/edit/[id].tsx
+++ src/pages/settings/restaurants/edit/[id].tsx
@@ -77,99 +77,95 @@ export default function EditRestaurantsPage({
     <div className='min-h-screen bg-gray-100 flex flex-col px-6 lg:px-8'>
       <div className='container min-w-full mx-auto'>
         <div className='mx-auto w-3/4'>
-          <Card>
-            <Card.Header>
-              <div className='justify-between text-left'>
-                <Card.Header.Title>Edit Restaurant</Card.Header.Title>
-              </div>
-            </Card.Header>
-
-            <div className='bg-gray-400'>
-              <div className='border-2 bg-white mt-8 py-8 px-6 shadow rounded-lg sm:px-10'>
-                <FormProvider {...formMethods}>
-                  <form
-                    className='mb-0 space-y-6'
-                    onSubmit={handleSubmit(submitHandler)}
-                  >
-                    <div>
-                      <label
-                        className={clsx(
-                          'block text-sm font-medium ',
-                          errors.restaurantName
-                            ? 'text-orange-700'
-                            : 'text-gray-700'
-                        )}
-                      >
-                        Name
-                      </label>
-                      <div className='mt-1'>
-                        <input
-                          type='text'
-                          className={clsx(
-                            errors.restaurantName &&
-                              'text-orange-700 border-orange-700'
-                          )}
-                          {...register('restaurantName', { required: true })}
-                        />
-                      </div>
-                    </div>
-
-                    <div>
-                      <label
-                        className={clsx(
-                          'block text-sm font-medium ',
-                          errors.restaurantName
-                            ? 'text-orange-700'
-                            : 'text-gray-700'
-                        )}
-                      >
-                        Cuisine
-                      </label>
-                      <div className='mt-1'>
-                        <Controller
-                          name='cuisine'
-                          control={control}
-                          rules={{
-                            required: true,
-                          }}
-                          render={({ field: { onChange, value } }) => (
-                            <Select
-                              styles={{
-                                control: (base) => ({
-                                  ...base,
-                                  borderColor:
-                                    errors.cuisine && 'rgba(194, 65, 12)',
-                                }),
-                              }}
-                              value={{ label: value }}
-                              options={Object.keys(CuisineMap).map((k) => ({
-                                label: k,
-                              }))}
-                              getOptionValue={({ label }) => label}
-                              onChange={(option) => {
-                                onChange(option?.label || null);
-                              }}
-                            />
-                          )}
-                        />
-                      </div>
-                    </div>
-
-                    <AddressComponent apiKey={apiKey} />
-
-                    <div>
-                      <button
-                        type='submit'
-                        className='w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-orange-600 hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500'
-                      >
-                        Save
-                      </button>
-                    </div>
-                  </form>
-                </FormProvider>
-              </div>
+          <Card.Header>
+            <div className='mt-4 text-left'>
+              <Card.Header.Title>Edit Restaurant</Card.Header.Title>
             </div>
-          </Card>
+          </Card.Header>
+
+          <div className='bg-white shadow mt-4 py-8 px-6 sm:px-10'>
+            <FormProvider {...formMethods}>
+              <form
+                className='mb-0 space-y-6'
+                onSubmit={handleSubmit(submitHandler)}
+              >
+                <div>
+                  <label
+                    className={clsx(
+                      'block text-sm font-medium ',
+                      errors.restaurantName
+                        ? 'text-orange-700'
+                        : 'text-gray-700'
+                    )}
+                  >
+                    Name
+                  </label>
+                  <div className='mt-1'>
+                    <input
+                      type='text'
+                      className={clsx(
+                        errors.restaurantName &&
+                          'text-orange-700 border-orange-700'
+                      )}
+                      {...register('restaurantName', { required: true })}
+                    />
+                  </div>
+                </div>
+
+                <div>
+                  <label
+                    className={clsx(
+                      'block text-sm font-medium ',
+                      errors.restaurantName
+                        ? 'text-orange-700'
+                        : 'text-gray-700'
+                    )}
+                  >
+                    Cuisine
+                  </label>
+                  <div className='mt-1'>
+                    <Controller
+                      name='cuisine'
+                      control={control}
+                      rules={{
+                        required: true,
+                      }}
+                      render={({ field: { onChange, value } }) => (
+                        <Select
+                          styles={{
+                            control: (base) => ({
+                              ...base,
+                              borderColor:
+                                errors.cuisine && 'rgba(194, 65, 12)',
+                            }),
+                          }}
+                          value={{ label: value }}
+                          options={Object.keys(CuisineMap).map((k) => ({
+                            label: k,
+                          }))}
+                          getOptionValue={({ label }) => label}
+                          onChange={(option) => {
+                            onChange(option?.label || null);
+                          }}
+                        />
+                      )}
+                    />
+                  </div>
+                </div>
+
+                <AddressComponent apiKey={apiKey} />
+
+                <div>
+                  <button
+                    type='submit'
+                    className='w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-orange-600 hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500'
+                  >
+                    Save
+                  </button>
+                </div>
+              </form>
+            </FormProvider>
+          </div>
         </div>
       </div>
     </div>

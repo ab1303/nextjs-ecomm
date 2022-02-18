13589350fa0a72036ec0f99fc5966029e21b31c0
Sanath Chakkarayan <sanath.cme@gmail.com>
2022-02-18 12:42:26 +1100

    feat(profile)!: update profile page style

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -4,6 +4,7 @@ import React, { ReactElement } from 'react';
 import { FormProvider, useForm } from 'react-hook-form';
 import { toast } from 'react-toastify';
 
+import Card from '@/components/card';
 import AuthorizedLayout from '@/components/layout/AuthorizedLayout';
 
 import { postData } from '@/utils/fetchHttpClient';
@@ -54,114 +55,108 @@ export default function ProfilePage({ user }: ProfilePageProps) {
     <div className='flex flex-col min-h-screen px-6 bg-gray-100 lg:px-8'>
       <div className='container min-w-full mx-auto'>
         <div className='w-3/4 mx-auto'>
-          <div>
-            <div className='h-20'></div>
-            <h2 className='text-3xl font-extrabold text-center text-gray-900'>
-              Profile
-            </h2>
-          </div>
-
-          <div className='grid grid-cols-4 gap-4 bg-gray-400'>
-            <div className='col-span-1 border-2'></div>
-            <div className='col-span-3 px-6 py-8 mt-8 bg-white border-2 rounded-lg shadow sm:px-10'>
-              <FormProvider {...formMethods}>
-                <form
-                  className='mb-0 space-y-6'
-                  onSubmit={handleSubmit(submitHandler)}
-                >
-                  <div>
-                    <label
-                      className={clsx(
-                        'block text-sm font-medium ',
-                        errors.firstname ? 'text-orange-700' : 'text-gray-700'
-                      )}
-                    >
-                      First Name
-                    </label>
-                    <div className='mt-1'>
-                      <input
-                        type='text'
-                        className={clsx(
-                          errors.firstname &&
-                            'text-orange-700 border-orange-700'
-                        )}
-                        {...register('firstname', { required: true })}
-                      />
-                    </div>
-                  </div>
-
-                  <div>
-                    <label
-                      className={clsx(
-                        'block text-sm font-medium ',
-                        errors.lastname ? 'text-orange-700' : 'text-gray-700'
-                      )}
-                    >
-                      Last Name
-                    </label>
-                    <div className='mt-1'>
-                      <input
-                        type='text'
-                        className={clsx(
-                          errors.lastname && 'text-orange-700 border-orange-700'
-                        )}
-                        {...register('lastname', { required: true })}
-                      />
-                    </div>
-                  </div>
-
-                  <div>
-                    <label
-                      className={clsx(
-                        'block text-sm font-medium ',
-                        errors.email ? 'text-orange-700' : 'text-gray-700'
-                      )}
-                    >
-                      Email
-                    </label>
-                    <div className='mt-1'>
-                      <input
-                        readOnly
-                        type='text'
-                        className={clsx(
-                          errors.email && 'text-orange-700 border-orange-700'
-                        )}
-                        {...register('email', { required: true })}
-                      />
-                    </div>
-                  </div>
-
-                  <div>
-                    <label
-                      className={clsx(
-                        'block text-sm font-medium ',
-                        errors.phone ? 'text-orange-700' : 'text-gray-700'
-                      )}
-                    >
-                      Phone
-                    </label>
-                    <div className='mt-1'>
-                      <input
-                        type='text'
-                        className={clsx(
-                          errors.phone && 'text-orange-700 border-orange-700'
-                        )}
-                        {...register('phone', { required: false })}
-                      />
-                    </div>
-                  </div>
-
-                  <div>
-                    <button
-                      type='submit'
-                      className='flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-orange-600 border border-transparent rounded-md shadow-sm hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500'
-                    >
-                      Save
-                    </button>
-                  </div>
-                </form>
-              </FormProvider>
+          <Card.Header>
+            <div className='mt-4 text-left'>
+              <Card.Header.Title>Profile</Card.Header.Title>
             </div>
+          </Card.Header>
+
+          <div className='px-6 py-8 mt-4 bg-white shadow sm:px-10'>
+            <FormProvider {...formMethods}>
+              <form
+                className='mb-0 space-y-6'
+                onSubmit={handleSubmit(submitHandler)}
+              >
+                <div>
+                  <label
+                    className={clsx(
+                      'block text-sm font-medium ',
+                      errors.firstname ? 'text-orange-700' : 'text-gray-700'
+                    )}
+                  >
+                    First Name
+                  </label>
+                  <div className='mt-1'>
+                    <input
+                      type='text'
+                      className={clsx(
+                        errors.firstname && 'text-orange-700 border-orange-700'
+                      )}
+                      {...register('firstname', { required: true })}
+                    />
+                  </div>
+                </div>
+
+                <div>
+                  <label
+                    className={clsx(
+                      'block text-sm font-medium ',
+                      errors.lastname ? 'text-orange-700' : 'text-gray-700'
+                    )}
+                  >
+                    Last Name
+                  </label>
+                  <div className='mt-1'>
+                    <input
+                      type='text'
+                      className={clsx(
+                        errors.lastname && 'text-orange-700 border-orange-700'
+                      )}
+                      {...register('lastname', { required: true })}
+                    />
+                  </div>
+                </div>
+
+                <div>
+                  <label
+                    className={clsx(
+                      'block text-sm font-medium ',
+                      errors.email ? 'text-orange-700' : 'text-gray-700'
+                    )}
+                  >
+                    Email
+                  </label>
+                  <div className='mt-1'>
+                    <input
+                      readOnly
+                      color='gray'
+                      type='text'
+                      className={clsx('text-gray-400')}
+                      {...register('email', { required: true })}
+                    />
+                  </div>
+                </div>
+
+                <div>
+                  <label
+                    className={clsx(
+                      'block text-sm font-medium ',
+                      errors.phone ? 'text-orange-700' : 'text-gray-700'
+                    )}
+                  >
+                    Phone
+                  </label>
+                  <div className='mt-1'>
+                    <input
+                      type='text'
+                      className={clsx(
+                        errors.phone && 'text-orange-700 border-orange-700'
+                      )}
+                      {...register('phone', { required: false })}
+                    />
+                  </div>
+                </div>
+
+                <div>
+                  <button
+                    type='submit'
+                    className='flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-orange-600 border border-transparent rounded-md shadow-sm hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500'
+                  >
+                    Save
+                  </button>
+                </div>
+              </form>
+            </FormProvider>
           </div>
         </div>
       </div>
